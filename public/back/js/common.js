5d85ae0c33cd2d6c5887b3da6e78c30c13dfbf90
bixia <429614835@qq.com>
2018-05-10 17:04:57 +0800

    登录验证完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,6 +1,25 @@
 /**
  * Created by lenovo on 2018/5/9.
  */
+//判断页面是否经过登录
+
+if(location.href.indexOf("login.html")==-1){
+  $.ajax({
+    url:'/employee/checkRootLogin',
+    type:'get',
+    dataType:'json',
+    success:function (info) {
+      console.log(info);
+      if(info.success){
+      
+      }else{
+        location.href="login.html";
+      }
+    }
+  })
+}
+
+
 // 禁用进度环
 NProgress.configure({ showSpinner: false });
 
@@ -18,19 +37,41 @@ $(document).ajaxStop(function() {
   }, 500);
 });
 
-// 分类管理
-$('.lt_aside .nav .category').click(function () {
-  $('.child').slideToggle();
-})
+
+  // 分类管理
+  $('.category').click(function() {
+    $(this).next().stop().slideToggle();
+  });
+  
+
 
 //点击菜单按钮，侧边栏隐藏
-$('.icon_menu').click(function () {
-  $('.lt_aside').toggleClass('hidemenu');
-  $('.lt_main').toggleClass('hidemenu');
-  $('.lt_main .main_top').toggleClass('hidemenu');
-})
+  $('.icon_menu').click(function () {
+    $('.lt_aside').toggleClass('hidemenu');
+    $('.lt_main').toggleClass('hidemenu');
+    $('.lt_main .main_top').toggleClass('hidemenu');
+  })
 
 //模态框
-$('.icon_logout').click(function () {
-  $('#logoutModal').modal('show');
-})
+  $('.icon_logout').click(function () {
+    $('#logoutModal').modal('show');
+  })
+  
+  //点击退出按钮
+  $('#logoutBtn').click(function () {
+    $.ajax({
+      url:'/employee/employeeLogout',
+      dataType:'json',
+      type:'get',
+      success:function (info) {
+        console.log(info);
+        if(info.success){
+          location.href="login.html";
+        }
+      }
+    })
+  })
+
+
+
+
